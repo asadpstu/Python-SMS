fddc8ac4ffb9d926978cf11ddffaebeb186997c3
Asad <hmasad09@gmail.com>
2018-12-11 02:54:16 +0800

    Student App Migration added

--- SchoolManagementSystem/settings.py
+++ SchoolManagementSystem/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'Student.apps.StudentConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- Student/models.py
+++ Student/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name  = models.CharField(max_length=30,blank=True, null=True,)
+    reg        = models.IntegerField()
+    GENDER = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('N', 'N/A'),
+    )
+    gender     = models.CharField(max_length=1, choices=GENDER)
+    dob        = models.DateField()
+    photo      = models.CharField(max_length=210,blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Result(models.Model):
+    student_id = models.IntegerField()
+    class_name = models.CharField(max_length=100,blank=False, null=False,)
+    year       = models.IntegerField()    
+    total_marks= models.IntegerField()  
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)	
+		
